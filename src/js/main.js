a17b785773e6d2c69b81d190bfe1eefc2ba7f54e
Anoesj <anoesjsadraee@gmail.com>
2021-12-01 03:40:21 +0100

    Adding project teasers

--- src/js/main.js
+++ src/js/main.js
@@ -26,6 +26,12 @@ import { backButton } from './components/back-button.js';
                   >
                     <router-view ref="route"/>
                   </transition>
+
+                  <footer class="social-links">
+                    <a href="https://www.linkedin.com/in/anoesj/"><i class="fab fa-linkedin"></i></a>
+                    <a href="https://twitter.com/anoesj"><i class="fab fa-twitter"></i></a>
+                    <a href="https://github.com/anoesj"><i class="fab fa-github"></i></a>
+                  </footer>
                 </div>`,
 
     data () {
--- src/js/routes/about.js
+++ src/js/routes/about.js
@@ -16,13 +16,21 @@ export const about = {
                 <div class="text" ref="text">
                   <h1>Hi, I’m Anoesj.</h1>
                   <p>A web developer mainly focused on creating slick, usable web apps. I am experienced with JavaScript, CSS, Vue.js, writing APIs and more. More importantly however, I’ve learned to <strong>really</strong> understand the clients I work with. Finding out what problems they are facing, gathering domain knowledge and working towards a sustainable solution.</p>
-                  <p>This is something I learned while working at <a href="https://fonkel.io" class="link">Fonkel</a>, where I worked on Drupal, Vue.js and Laravel projects on and off in the past decade. In the meantime, I finished my Bachelor of Music in 2020 at ArtEZ Institute of the Arts in Enschede, The Netherlands. During my (audio related) internship at <a href="https://media.monks.com/" class="link">Media.Monks</a>, I realized my interest lies mainly in programming. Since then, I’ve started freelancing alongside working at Fonkel.</p>
-                  <p>As you can see, this website is fairly incomplete. A new one is in the works, but for now, you can reach me on any of the following platforms:</p>
+                  <h3>Background</h3>
+                  <p>I learned so much at <a href="https://fonkel.io" class="link">Fonkel</a>, where I worked on Drupal, Vue.js and Laravel projects on and off in the past decade. In the meantime, I finished my Bachelor of Music in 2020 at ArtEZ Institute of the Arts in Enschede, The Netherlands. During my (audio related) internship at <a href="https://media.monks.com/" class="link">Media.Monks</a>, I realized my interest lies mainly in programming. Since then, I’ve started freelancing alongside working at Fonkel.</p>
+                  <!-- TODO: Maybe move this to a sticky footer instead? -->
+                  <!-- <p>As you can see, this website is fairly incomplete. A new one is in the works, but for now, you can reach me on any of the following platforms:</p>
                   <p class="social-links">
                     <a href="https://www.linkedin.com/in/anoesj/"><i class="fab fa-linkedin"></i></a>
                     <a href="https://twitter.com/anoesj"><i class="fab fa-twitter"></i></a>
                     <a href="https://github.com/anoesj"><i class="fab fa-github"></i></a>
-                  </p>
+                  </p> -->
+                </div>
+
+                <div class="featured-projects" ref="featuredProjects">
+                  <router-link :to="{ name: 'project--haringvliet' }">Haringvliet</router-link>
+                  <router-link :to="{ name: 'project--kozijnverbindingen' }">Kozijnverbindingen</router-link>
+                  <router-link :to="{ name: 'project--groenlinks-maak' }">GroenLinks Maak</router-link>
                 </div>
               </section>`,
 
